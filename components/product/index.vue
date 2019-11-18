2a62e9b63e537955df0a433621d39e467e132058
YOUS-DELL <yous.yoeunfrence@gmail.com>
2019-11-18 22:04:24 +0700

    tag new

--- components/product/index.vue
+++ components/product/index.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="product-container">
-    <v-card hover class="max-auto">
-      <v-img height="100%" width="100%" :src="loadImage(dataSrc.img)"></v-img>
+    <v-card class="mx-auto" hover>
+      <div class="ribbon">
+        <div class="label">NEW</div>
+      </div>
+      <v-img :src="loadImage(dataSrc.img)"></v-img>
       <v-spacer />
       <v-list-item>
         <v-list-item-content>
@@ -57,26 +60,80 @@ export default {
     line-height: 2rem;
     letter-spacing: normal;
   }
+
   .v-card {
     .price {
       font-size: 2rem;
-      font-weight 200
+      font-weight: 200;
     }
-      .btn-wrapper {
-        button {
-          width : 52%;
-        }
-        width: 100%;
-        position : absolute;
-        display: none;
-        top:45%;
-        left: 23%;
+
+    .btn-wrapper {
+      button {
+        width: 52%;
+      }
+
+      width: 100%;
+      position: absolute;
+      display: none;
+      top: 45%;
+      left: 23%;
     }
 
     &:hover {
       .btn-wrapper {
-          display: initial;
+        display: initial;
       }
+    }
+
+  /*  ================================================
+                      RIBBON EFFECT
+      ================================================  */
+    .ribbon {
+      position: absolute;
+      display: block;
+      top: -4px;
+      right: -4px;
+      width: 110px;
+      height: 110px;
+      overflow: hidden;
+      z-index 1
+    }
+    .ribbon .label {
+      position: relative;
+      display: block;
+      left: -10px;
+      top: 23px;
+      width: 158px;
+      padding: 10px 0;
+      font-size: 15px;
+      text-align: center;
+      color: #fff;
+      background-color: #e85e68;
+      -webkit-box-shadow: 0px 0px 4px rgba(0,0,0,0.3);
+      -moz-box-shadow: 0px 0px 4px rgba(0,0,0,0.3);
+      -ms-box-shadow: 0px 0px 4px rgba(0,0,0,0.3);
+      box-shadow: 0px 0px 4px rgba(0,0,0,0.3);
+      -webkit-transform: rotate(45deg) translate3d(0,0,0);
+      -moz-transform: rotate(45deg) translate3d(0,0,0);
+      -ms-transform: rotate(45deg) translate3d(0,0,0);
+      transform: rotate(45deg) translate3d(0,0,0);
+    }
+
+    .label:before, .label:after {
+      content: '';
+      position: absolute;
+      bottom: -4px;
+      border-top: 4px solid #a71c26;
+      border-left: 4px solid transparent;
+      border-right: 4px solid transparent;
+    }
+
+    .label:before {
+      left: 0;
+    }
+
+    .label:after {
+      right: 0;
     }
   }
 }
@@ -84,8 +141,8 @@ export default {
 <style lang="stylus">
 .product-container {
   .v-card .btn-wrapper {
-     button {
-      font-size: .8rem;
+    button {
+      font-size: 0.8rem;
       font-weight: 400;
     }
   }
